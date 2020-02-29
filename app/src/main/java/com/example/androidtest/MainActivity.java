0b88bf87572704c62fcedc81ab35d70c98d79c6a
StevenKit <544736192@qq.com>
2020-02-29 14:22:32 -0600

    This is just a test for this function

---
+++ app/src/main/java/com/example/androidtest/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.androidtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.widget.Button;
+import android.util.Log;
+import android.widget.EditText;
+import android.content.SharedPreferences;
+import android.view.View;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText txtContent, txtTitle;
+    int listNumber = 0;
+    Button btnGo;
+    String listNote = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+        btnGo = findViewById(R.id.btnGo);
+        txtTitle = findViewById(R.id.txtTitle);
+        txtContent = findViewById(R.id.txtContent);
+
+        btnGo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //the list of note for calculation;
+                listNumber++;
+
+                listNote = "note";
+                NoteModel note = new NoteModel();
+
+                //for the edit the content
+                note.title = txtTitle.getText().toString();
+                note.content = note.title;
+
+                //open the preferences(contentWrite), if exit, open it, if it not exit, create a new
+                SharedPreferences contentWrite = getPreferences(0);
+
+                //let contentWrite in the state of editing.
+                SharedPreferences.Editor editor = contentWrite.edit();
+                String json = note.toJson();
+                Log.i("json", json);
+                note.fromJson(json);
+
+                //this is for storing the data.
+                editor.putString(note.title, json);
+
+                //submit the data
+                editor.commit();
+                txtTitle.setText(note.title);
+
+                //the final content
+                txtContent.setText(txtContent.getText().toString() + listNote + " "
+                        + listNumber +":  " + note.content+"\n" + "Edit :"+ "\n" + "\n");
+            }
+        });
+
+    }
+}
